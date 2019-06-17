0052ada737f243722e9862e86d44204f125854a9
vipul-vinsol <50447641+vipul-vinsol@users.noreply.github.com>
2019-06-17 18:07:49 +0530

    Update class_module.rb

--- class_module.rb
+++ class_module.rb
@@ -1,33 +1,33 @@
 module ClassModule
 
   def action_methods(*args)
-    if args.any? { |ele| public_method_defined?(ele) }
-      args.each do |m_sym|
-        temp_method = instance_method(m_sym)
-        store = get_filter_store
-        define_method(m_sym) do
-          store[m_sym][:before].each { |before_method| send before_method }
-          temp_method.bind(self).call
-          store[m_sym][:after].each { |after_method| send after_method }
-        end
+    args.any? { |ele| public_method_defined?(ele) } && args.each do |m_sym|
+      temp_method = instance_method(m_sym)
+      store = get_filter_store
+      define_method(m_sym) do
+        store[m_sym][:before].each { |before_method| send before_method }
+        temp_method.bind(self).call
+        store[m_sym][:after].each { |after_method| send after_method }
       end
     end
   end
 
   def before_filter(*args, only: nil, except: nil)
-    return false unless args.all? { |ele| private_method_defined?(ele) }
-    return if only && insert_filters_in_store(only, :before, args)
-    filter_on_methods = filter_methods 
-    except && filter_on_methods -= except
-    insert_filters_in_store(filter_on_methods, :before, args)
+    if args.all? { |ele| private_method_defined?(ele) }
+      return if only && insert_filters_in_store(only, :before, args)
+      filter_on_methods = filter_methods 
+      except && filter_on_methods -= except
+      insert_filters_in_store(filter_on_methods, :before, args)
+    end
   end
 
   def after_filter(*args, only: nil, except: nil)
-    return false unless args.all? { |ele| private_method_defined?(ele) }
-    return if only && insert_filters_in_store(only, :after, args)
-    filter_on_methods = filter_methods
-    except && filter_on_methods -= except
-    insert_filters_in_store(filter_on_methods, :after, args)
+    if args.all? { |ele| private_method_defined?(ele) }
+      return if only && insert_filters_in_store(only, :after, args)
+      filter_on_methods = filter_methods
+      except && filter_on_methods -= except
+      insert_filters_in_store(filter_on_methods, :after, args)
+    end
   end
 
   private def get_filter_store
